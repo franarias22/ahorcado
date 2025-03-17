e4a8971058f9a47adac10d538c8c37d46edd6d80
franarias22 <francisco112206@gmail.com>
2025-03-17 19:56:43 -0300

    EliminacionScannerInnecesario

--- src/ahorcado/AhorcadoJuego.java
+++ src/ahorcado/AhorcadoJuego.java
@@ -12,39 +12,38 @@ import java.util.Scanner;
  */
 
 public class AhorcadoJuego {
-    
+
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in); // Único objeto Scanner para todo el código
         System.out.println("¿Cuántas letras tiene su palabra?");
-	Scanner tampalScan = new Scanner(System.in);
-        int tampal = tampalScan.nextInt();
-	Ahorcado A = new Ahorcado();
-	A.reducirtamanio(tampal);
-
-	//Repite el proceso de imprimir hasta que vida sea 0, posib sea 1 o 0.
-	while(A.getVida() > 0 && A.getPosib() != 1 && A.getPosib() != 0){
-            A.imprimir();
-	}
-	 
-        //Dice cual es la palabra encontrada.
-	if(A.getPosib() == 1)
-            for(int i = 0; i < A.getCantidadD(); i++)
-                if(A.getElemSoN(i) == true)
-                    System.out.println("La palabra es " + A.getElemD(i));
+        int tampal = scanner.nextInt();
         
-	//No se encontro la palabra.
-	if(A.getPosib() == 0)
+        Ahorcado A = new Ahorcado();
+        A.reducirtamanio(tampal);
+
+        // Repite el proceso de imprimir hasta que vida sea 0, posib sea 1 o 0.
+        while (A.getVida() > 0 && A.getPosib() != 1 && A.getPosib() != 0) {
+            A.imprimir();
+        }
+
+        // Dice cual es la palabra encontrada.
+        if (A.getPosib() == 1)
+            for (int i = 0; i < A.getCantidadD(); i++)
+                if (A.getElemSoN(i) == true)
+                    System.out.println("La palabra es " + A.getElemD(i));
+
+        // No se encontró la palabra.
+        if (A.getPosib() == 0)
             System.out.println("La palabra no se encuentra en el diccionario.");
 
-	//El programa perdio.
-	if(A.getVida() == 0)
+        // El programa perdió.
+        if (A.getVida() == 0)
             System.out.println("Has ganado el juego. ¡Felicidades!");
-	
+
         System.out.println("Fin del juego.");
         
         System.out.println("Presione ENTER para terminar...");
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
+        scanner.nextLine(); // Utilizo el mismo objeto scanner para esperar la entrada
     }
-    
 }
